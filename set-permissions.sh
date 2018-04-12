daaa9329c48c80e0bd2b41b5d5bead66b35c6883
Sam Kohn <kohn@berkeley.edu>
2018-04-12 15:57:11 -0700

    Add simple script to set appropriate read permissions

---
+++ set-permissions.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/sh
+
+CORE_FILES="js/main.js index.html"
+GEOM_FILES="sensor_plane_28_full.txt sensor_plane_28_simple.txt"
+DATA_FILES="bern-1.json data/*.json"
+
+chmod a+r $CORE_FILES $GEOM_FILES $DATA_FILES

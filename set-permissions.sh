12b6d6bcedbdf652f1b34bd5e7e91e52fba769d6
Sam Kohn <kohn@berkeley.edu>
2018-04-16 15:59:13 -0700

    Add URI.js library into webpage

--- set-permissions.sh
+++ set-permissions.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/sh
 
-CORE_FILES="js/main.js index.html"
+CORE_FILES="js/main.js js/lib/*.js js/lib/three/*.js index.html"
 GEOM_FILES="sensor_plane_28_full.txt sensor_plane_28_simple.txt"
 DATA_FILES="bern-1.json data/*.json"
 

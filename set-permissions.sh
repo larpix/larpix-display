62290cdef602c7231192ea6f6531c19c2c5db19a
Sam Kohn <kohn@berkeley.edu>
2018-04-19 22:13:47 -0700

    Update set-permissions.sh to include directory permissions

--- set-permissions.sh
+++ set-permissions.sh
@@ -4,4 +4,7 @@ CORE_FILES="js/main.js js/lib/*.js js/lib/three/*.js index.html"
 GEOM_FILES="geometries/*.txt"
 DATA_FILES="data/*.json"
 
+DIRECTORIES="data geometries js js/lib js/lib/three"
+
 chmod a+r $CORE_FILES $GEOM_FILES $DATA_FILES
+chmod a+x $DIRECTORIES
